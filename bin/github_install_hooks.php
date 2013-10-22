7036e17f70b73974e4f751ea2b19ff8569e60c43
Zak James <zak.james@gmail.com>
2013-10-22 16:55:33 -0400

    hook installer command line organization override, auto repo enumeration
    -added an option to set the organization on the command line,
    overriding the config file setting.
    -if no repos are listed in the configuration, the script now fetches
    all owned by the org and applies the hook to each.

--- bin/github_install_hooks.php
+++ bin/github_install_hooks.php
@@ -8,14 +8,44 @@ if (file_exists('../config/projects_local.php')) {
 } else {
   include('../config/projects.php');
 }
+include('../lib/restclient.php');
+
+$client = new RestClient(GITHUB_ENDPOINT_URL);
 
 if (!defined('GITHUB_TOKEN')) {
   exit('You must provide a Github access token environment variable to install webhooks.');
 }
-if (!count($github_projects)) {
-  error('You must add project names for one or more github repositories to config/projects.php.');
+
+//search for command line passed github organization to monitor
+$options = getopt("o::", array('organization::'));
+if ($options['organization']) {
+  $github_organization = $options['organization'];
 }
-//TODO: if the projects list is empty, enumerate all organization repos
+if ($options['o']) {
+  $github_organization = $options['o'];
+}
+if ($github_organization == '') {
+  exit("You must provide a Github organization as a target for webhook installation in the configuration file, or on the command line as -o=[name] or --organization=[name]\n");
+}
+
+if (!count($github_projects)) {
+  //if the projects list is empty, enumerate all organization repos
+  error_log('no github repositories listed in config/projects.php, enumerating all repos.');
+  $url = implode('/', array(
+    GITHUB_ENDPOINT_URL,
+    'orgs',
+    $github_organization,
+    'repos'
+  ));
+
+  $result = $client->get($url);
+  if (is_array($result)) {
+    for ($i=0; $i < count($result); $i++) { 
+      $github_projects[] = $result[$i]->name;
+    }
+    error_log('repo list: '. print_r($github_projects, true));
+  }
+}
 
 //create payload required for github hook post
 //see http://developer.github.com/v3/repos/hooks/#create-a-hook
@@ -41,13 +71,10 @@ for ($i=0; $i < count($github_projects); $i++) {
     'hooks'
   ));
 
-  $json = json_encode($payload);
-
-  $result = curl_post($patchUrl, $json);
+  $resultObj = $client->post($patchUrl, $payload);
   
   //report success or error
-  if ($result) {
-    $resultObj = json_decode($result);
+  if ($resultObj) {
     if ($resultObj->url) {
       echo('installed webhook: ' . $resultObj->url . "\n");      
     } else {
